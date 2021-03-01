e4769a609b5de88be25b01c62d628e11efbe4370
khaedzi <khaled.ziadeh@yahoo.com>
2021-03-01 15:19:06 +0200

    firstpart

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const cors = require('cors');
 const superAgent = require('superagent');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-
+const pg = require('pg');
 const app = express();
+let client =new pg.Client(process.env.databaseUrl);
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 app.use(cors());
 app.use(bodyParser())
 const baseAPIUrl = 'https://www.googleapis.com/books/v1/volumes';
 
-app.get('/', (req, res) =>{
-    res.render('pages/index');
-})
+
+app.get("/",handleHome)
+
 
 app.get('/searches/new', (req, res) =>{
     res.render('pages/searches/new')
@@ -23,9 +24,8 @@ app.get('/searches/new', (req, res) =>{
 
 app.post('/searches', handleSearch)
 
-app.listen(process.env.PORT, () =>{
-    console.log("Listening on port " + process.env.PORT)
-})
+
+
 
 function Book(img_url, title, author_name, description){
     this.img_url = img_url || `https://i.imgur.com/J5LVHEL.jpg`;
@@ -33,9 +33,23 @@ function Book(img_url, title, author_name, description){
     this.authorName = author_name;
     this.description = description || '';
 }
+  
+function handleHome(req,res){
+getdataFromDb().then(data=>{
+
+    res.render('pages/index',{data:data,length:data.length});
+});
+}
+
+function getdataFromDb(){
+    
+    let myData="select * from favourite;"
+    return client.query(myData).then(data => {
+    return data.rows;
+})}
 
 function handleSearch(req, res){
-    getBooksData(req.body.searchQuery, req.body.searcBy).then(data =>{
+    getBooksData(req.body.searchQuery, req.body.searchBy).then(data =>{
         console.log("data", data)
         res.render('pages/searches/show', {books:data})
     }).catch(error => res.render('pages/error', {error:error}))
@@ -56,3 +70,11 @@ function getBooksData(searchQuery, searchBy){
         })
     }).catch(error => res.render('pages/error', {error:error}))
 }
+
+client.connect().then(()=>{
+
+app.listen(process.env.PORT, () =>{
+    console.log("Listening on port " + process.env.PORT)
+})}).catch(error=>
+    console.log("cant conected databas"))
+
